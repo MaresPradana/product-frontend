f3767f25062b9e9b7e5dddb099ee47f6c50af1ce
Mares <maresbejo@gmail.com>
2024-11-26 20:19:18 +0700

    product-hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import CreateProduct from '../views/CreateProduct.vue';
 import EditProduct from '../views/EditProduct.vue';
@@ -22,7 +22,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/product-frontend/'),
   routes,
 });
 
